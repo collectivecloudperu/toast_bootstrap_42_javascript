fb8dd1e5cdd47a07343cdf720ba9eb6107025b98
Nube Colectiva <collectivecloudperu@users.noreply.github.com>
2019-09-20 20:01:05 -0500

    Add files via upload

--- js/app.js
+++ js/app.js
@@ -1,11 +1,10 @@
-// Con esta función se muestra el Toast 
+
 function mostrarToast() {
     var toast = document.getElementById("mitoast");
     toast.className = "mostrar";
     setTimeout(function(){ toast.className = toast.className.replace("mostrar", ""); }, 5000);
 }
 
-// Con esta función se cierra el Toast 
 function cerrarToast() {
     var toast = document.getElementById("mitoast");
     toast.className = "cerrar";
